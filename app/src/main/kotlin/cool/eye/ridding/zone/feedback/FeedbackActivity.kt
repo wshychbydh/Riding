b13b09da01544c352666c69333be2cfe58d941ad
yangchuanbo <443495586@qq.com>
2017-02-20 15:22:18 +0800

    修复意见反馈字数显示bug

--- app/src/main/kotlin/cool/eye/ridding/zone/feedback/FeedbackActivity.kt
+++ app/src/main/kotlin/cool/eye/ridding/zone/feedback/FeedbackActivity.kt
@@ -1,6 +1,8 @@
 package cn.sudiyi.app.client.account.page
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import cn.bmob.v3.BmobUser
 import cn.sudiyi.app.client.account.support.FeedbackPickerDialog
@@ -39,6 +41,17 @@ class FeedbackActivity : BaseActivity() {
         } else {
             feedback_contacts.setText(user.mobilePhoneNumber)
         }
+        feedback_content.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                updateFeedbackCount(feedback_content.length())
+            }
+        })
     }
 
     private fun submit() {
@@ -77,10 +90,16 @@ class FeedbackActivity : BaseActivity() {
     }
 
     private fun updateFeedbackCount(count: Int) {
-        feedback_content_count.text = "$count / $MAX_COUNT"
+        if (count > MAX_COUNT - RED_COUNT) {
+            feedback_content_count.text = Utils.formatPartColorOfStr(resources.getColor(R.color.red),
+                    "$count / $MAX_COUNT", count.toString())
+        } else {
+            feedback_content_count.text = "$count / $MAX_COUNT"
+        }
     }
 
     companion object {
         private val MAX_COUNT = 200
+        private val RED_COUNT = 50
     }
 }
